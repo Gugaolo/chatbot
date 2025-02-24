ee962de750cfd82884675f9b55aff088a401178e
Gal Stravs <gal.stravs@gmail.com>
2025-02-24 21:38:28 +0100

    Fixed frontend module error

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  
 import google.generativeai as genai
 import fitz  # PyMuPDF for extracting text from PDF
+import frontend
+
 
 app = Flask(__name__)
 CORS(app)  
