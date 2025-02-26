7083ec89cc385a2ccd4dbe080909afaa83c8dee3
Gal Stravs <gal.stravs@gmail.com>
2025-02-26 17:14:36 +0100

    Please work

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  
 import google.generativeai as genai
-import fitz  # PyMuPDF for extracting text from PDF
+import pymupdf  # PyMuPDF for extracting text from PDF
 
 
 app = Flask(__name__)
